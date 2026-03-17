70b6b9ea100b6ff2c07d437a2a671cf66cea9c13
HanadaB <bonyanihoda@gmail.com>
2026-03-17 14:27:27 +0100

    Problem 4 is solved

---
+++ 04_fib/fib.py
@@ -0,0 +1,16 @@
+"""Fibonacci sequence """
+
+def rabbits(n: int , k: int) -> int:
+    if n == 1 or n == 2  :
+        return 1
+
+    return rabbits(n-1, k) + k * rabbits(n-2 , k)
+
+
+def main():
+    n, k = map(int, input().split())
+    print(f"The population of rabbits after {n} month is: {rabbits(n, k)}")
+
+
+if __name__ == "__main__":
+    main()
